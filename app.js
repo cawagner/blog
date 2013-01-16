024ae7732db1036ef9d31bc1aeb94bad052f9668
Chris Wagner <eelhovercraft@gmail.com>
2013-01-16 01:15:16 -0500

    Much more stylish

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ var poet         = require('poet')(app);
 var moment     = require('moment');
 
 poet.set({
-    postsPerPage : 3,
-    posts                : './_posts',
-    metaFormat     : 'json'
+    postsPerPage: 3,
+    posts: './_posts',
+    metaFormat: 'json',
+    readMoreLink : function ( post ) {
+      var anchor = '<a href="'+post.url+'" title="Read more of '+post.title+'">read more</a>';
+      return '<p>' + anchor + '</p>';
+    },
 }).createPostRoute( '/posts/:post', 'post' )
     .createPageRoute( '/page/:page', 'page' )
     .createTagRoute( '/tags/:tag', 'tag' )
