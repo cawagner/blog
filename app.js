37ef77c38e14b91d9139dae184304565487b5407
Chris Wagner <eelhovercraft@gmail.com>
2013-01-15 23:56:47 -0500

    Initial state mostly working

--- app.js
+++ app.js
@@ -1,36 +1,24 @@
+'use strict';
 
-/**
- * Module dependencies.
- */
+var express  = require('express');
+var app      = express();
+var poet     = require('poet')(app);
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+poet.set({
+  postsPerPage : 3,
+  posts        : './_posts',
+  metaFormat   : 'json'
+}).createPostRoute( '/myposts/:post', 'post' )
+  .createPageRoute( '/pagination/:page', 'page' )
+  .createTagRoute( '/mytags/:tag', 'tag' )
+  .createCategoryRoute( '/mycategories/:category', 'category' )
+  .init();
 
-var app = express();
+app.set( 'view engine', 'jade' );
+app.set( 'views', __dirname + '/views' );
+app.use( express.static( __dirname + '/public' ));
+app.use( app.router );
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(path.join(__dirname, 'public')));
-});
+app.get( '/', function ( req, res ) { res.render( 'index' ) });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
-
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
+app.listen( 3000 );
