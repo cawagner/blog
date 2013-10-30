5c7eb91a2d6f18fac93029ae69bbbcd109503825
Chris Wagner <eelhovercraft@gmail.com>
2013-10-29 21:17:38 -0400

    Upgraded to newest version of poet

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
 'use strict';
 
 var express = require('express');
-var app = express();
-var poet = require('poet')(app);
 var moment = require('moment');
+var Poet = require('poet');
 
-poet.set({
+var app = express();
+
+var poet = Poet(app, {
     postsPerPage: 3,
     posts: './_posts',
     metaFormat: 'json',
     readMoreLink : function ( post ) {
       var anchor = '<a href="'+post.url+'" title="Read more of '+post.title+'">read more</a>';
       return '<p>' + anchor + '</p>';
-    },
-}).createPostRoute( '/posts/:post', 'post' )
-    .createPageRoute( '/page/:page', 'page' )
-    .createTagRoute( '/tags/:tag', 'tag' )
-    .createCategoryRoute( '/categories/:category', 'category' )
-    .init();
+    }
+});
+
+poet.init();
 
 app.locals.moment = moment;
 
-app.set( 'view engine', 'jade' );
-app.set( 'views', __dirname + '/views' );
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
 
 app.use(require('stylus').middleware(__dirname + '/public'));
 app.use(express.compress());
 app.use(express.static(__dirname + '/public'));
 app.use(app.router);
 
-app.get( '/', function ( req, res ) { res.render( 'index' ) });
+app.get('/', function (req, res) {
+    res.render('index');
+});
 
 app.listen( 3000 );
